94ccd0059a92ebcb8f8ed4caafc032b114e6d9b0
Saurabhkale281 <kalesaurabh281@gmail.com>
2024-01-15 16:53:43 +0530

    nano .git/COMMIT_EDITMSG

--- JS/fetch_questions.js
+++ JS/fetch_questions.js
@@ -1,7 +1,5 @@
-function getQuestions(){
+async function getQuestions() {
 
-  //initializing empty array of question and answer object
-  let questions = [];
 
   var myHeaders = new Headers();
   myHeaders.append("Cookie", "PHPSESSID=8dc7a956c66cfa0c194077f273b39806");
@@ -11,12 +9,12 @@ var requestOptions = {
   headers: myHeaders,
   redirect: 'follow'
 };
-
-
-
-  return fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple", requestOptions)
+  return await fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple", requestOptions)
   .then(response => response.json())
   .then(result => result)
   .catch(error => console.log('error', error));;
 }
+
+
+getQuestions().then((res)=> console.log(res['results']))
 
