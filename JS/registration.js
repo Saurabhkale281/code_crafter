29392504d1f8b9f76934e2dbbcf210fb1b24f06c
Saurabhkale281 <kalesaurabh281@gmail.com>
2024-01-18 00:17:22 +0530

    nano .git/COMMIT_EDITMSG

--- JS/registration.js
+++ JS/registration.js
@@ -1,3 +1,23 @@
+
+// function seterror(id, error) {
+//     //sets error inside tag of id
+
+//     element = document.getElementById(id);
+//     element.getElementsByClassName('formerror')[0].innerHTML = error;
+// }
+
+// function validateForm() {
+//     var returnval = true;
+
+//     // perform validation if validation fail, set the value returnval to false
+//     var first_name = document.forms['myForm']["first_name"].value;
+//     if (name.length < 5) {
+//         seterror("first_name", "length of email is too short");
+//         returnval = false;
+//     }
+//     return returnval;
+// }
+
 function handleRegistration(){
     let email = document.getElementById("email_id").value; 
     let password = document.getElementById("password").value;
@@ -9,6 +29,5 @@ function handleRegistration(){
     // console.log(gender);
 
     alert("Hey! "+fname+" "+lname+", "+" you are now part of Code Crafters" );
-
-
 }
+
